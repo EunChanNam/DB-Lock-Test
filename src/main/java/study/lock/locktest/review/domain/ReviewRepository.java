719341e9a1531b0e4265533a326e69f6ed65fef3
EunChanNam <namhm23@gmail.com>
2023-04-18 15:09:55 +0900

    비관적 락을 적용해서 동시성 이슈 해결

--- src/main/java/study/lock/locktest/review/domain/ReviewRepository.java
+++ src/main/java/study/lock/locktest/review/domain/ReviewRepository.java
@@ -1,15 +1,17 @@
 package study.lock.locktest.review.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.persistence.LockModeType;
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    @Query("select distinct r from Review r " +
-            "left join fetch r.likeReviews " +
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select r from Review r " +
             "where r.id = :id")
     Optional<Review> findWithLikeById(@Param("id") Long id);
 }
